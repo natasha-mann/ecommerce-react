4390301431401e68262c0e1cf1106a4894646628
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-08-10 17:33:45 +0100

    set up react app with apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router } from "react-router-dom";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+
+import "./App.css";
+
+const httpLink = createHttpLink({
+  uri: process.env.GRAPHQL_URL || "http://localhost:4000/",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="App"></div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
