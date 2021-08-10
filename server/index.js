ea6669cfac287e79268d1c5b6e3f4231b4102f0a
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-08-10 14:20:02 +0100

    added basic apollo server

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const { ApolloServer } = require("apollo-server");
+
+const db = require("./config/connection");
+
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+db.once("open", () => {
+  server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
+});
