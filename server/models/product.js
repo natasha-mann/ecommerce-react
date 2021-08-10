8135c549ebe3d0cd36c91259fb2c7453b83158ea
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-08-10 15:27:15 +0100

    added mongoose model

---
+++ server/models/product.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require("mongoose");
+
+const schema = {
+  type: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  style: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  color: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  sizes: [
+    {
+      size: { type: Number, required: true },
+      stock: { type: Number, default: 0 },
+    },
+  ],
+};
+
+const productSchema = new Schema(schema);
+
+const Product = model("Product", productSchema);
+
+module.exports = Product;
