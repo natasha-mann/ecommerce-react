57aaf88af2500c490fd42051640e3771cbcf57ea
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-08-16 22:59:37 +0100

    updated discount function

--- client/src/pages/Basket/index.js
+++ client/src/pages/Basket/index.js
@@ -1,12 +1,35 @@
+import { useState } from "react";
 import BasketItem from "../../components/BasketItem";
 import Title from "../../components/Title";
 import calculateSubtotal from "../../utils/calculateSubtotal";
 import "./Basket.css";
 
 const Basket = () => {
+  const [isValidDiscount, setIsValidDiscount] = useState(null);
+  const [discountedTotal, setDiscountedTotal] = useState();
   const products = JSON.parse(localStorage.getItem("cart")) || [];
 
   const renderBasket = () => {
+    const subtotal = calculateSubtotal(products);
+
+    const handleApplyDiscount = (event) => {
+      event.preventDefault();
+
+      if (event.currentTarget.discount.value === "NEW15") {
+        const discountedSubtotal = (subtotal / 100) * 85;
+        setDiscountedTotal(discountedSubtotal);
+        setIsValidDiscount(true);
+      } else if (event.currentTarget.discount.value === "NEW20") {
+        const discountedSubtotal = (subtotal / 100) * 80;
+        setDiscountedTotal(discountedSubtotal);
+        setIsValidDiscount(true);
+      } else {
+        setDiscountedTotal(subtotal);
+        setIsValidDiscount(false);
+        return <div>Total: £ {subtotal}</div>;
+      }
+    };
+
     if (products.length) {
       const cards = products.map((product) => (
         <BasketItem
@@ -26,15 +49,33 @@ const Basket = () => {
               <hr />
 
               <div className="summary-info">
-                <div>Subtotal: £ {calculateSubtotal(products)}</div>
+                <div>Subtotal: £ {subtotal}</div>
+                {isValidDiscount && <div>Total: £ {discountedTotal}</div>}
                 <div>Shipping: FREE!</div>
                 <div>Estimated Delivery Time: 3 days</div>
               </div>
 
-              <div className="discount-container text-center">
+              <form
+                onSubmit={handleApplyDiscount}
+                className="discount-container text-center"
+              >
+                {isValidDiscount === false && (
+                  <div className="invalid-discount">
+                    Oops, invalid discount code!
+                  </div>
+                )}
                 <div className="my-2">ENTER DISCOUNT CODE:</div>
-                <input className="discount-input" type="text"></input>
-              </div>
+                <div className="discount-input-group">
+                  <input
+                    name="discount"
+                    className="discount-input"
+                    type="text"
+                  ></input>
+                  <button className="apply-button" type="submit">
+                    APPLY
+                  </button>
+                </div>
+              </form>
 
               <div className="confirm-button-div text-center">
                 <button className="confirm-button">CHECKOUT</button>
